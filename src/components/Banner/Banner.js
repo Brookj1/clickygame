7c130543a4353d0cd46a2ea2dbcf578a4452593f
Brookj1 <42814586+Brookj1@users.noreply.github.com>
2019-02-25 18:17:19 -0700

    intial working game

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on each image only once!</p>
+  </div>
+);
+export default Banner;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import blackcat from './images/blackcat.png';
+import blondecat from './images/blondecat.png';
+import curlycat from './images/curlycat.png';
+import gingerwig from './images/gingerwig.png';
+import glassescat from './images/glassescat.png';
+import greyhair from './images/greyhair.png';
+import pinkhat from './images/pinkhat.png';
+import pinkwig from './images/pinkwig.png';
+import princesscat from './images/princesscat.png';
+import redcat from './images/redcat.png';
+import rockercat from './images/rockercat.png';
+import smooshface from './images/smooshface.png';
+
+const images = [
+  blackcat,
+  blondecat,
+  curlycat,
+  gingerwig,
+  glassescat,
+  greyhair,
+  pinkhat,
+  pinkwig,
+  princesscat,
+  redcat,
+  rockercat,
+  smooshface
+];
+
+export default images;
